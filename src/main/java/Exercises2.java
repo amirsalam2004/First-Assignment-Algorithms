e9902e413c85f7f4dd773c3c18c0902f3813bdc1
Amir Hossein Ziaeifar <amirsalam2004@gmail.com>
2024-02-25 15:36:29 +0330

    Two Sums

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -14,8 +14,14 @@ public class Exercises2 {
     */
 
     public int[] twoSum(int[] nums, int target) {
-        // TODO
-        return null;
+        int[] houses=new int[2];
+        for(int i=0;i< nums.length-1;i++)
+            for(int j=i+1;j< nums.length;j++)
+                if(nums[i]+nums[j]==target){
+                   houses[0]=i;
+                   houses[1]=j;
+                }
+        return houses;
     }
 
     /*
